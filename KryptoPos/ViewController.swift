f8d74d6a231dc4ac7ea9660971e88bdd1d55f241
riyandafebri <febry.priyangga@gmail.com>
2020-02-06 11:40:41 +0700

    Initial Commit

---
+++ KryptoPos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KryptoPos
+//
+//  Created by Riyanda Febri Priyangga on 06/02/20.
+//  Copyright © 2020 Riyanda Febri Priyangga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KryptoPosTests/KryptoPosTests.swift
@@ -0,0 +1,34 @@
+//
+//  KryptoPosTests.swift
+//  KryptoPosTests
+//
+//  Created by Riyanda Febri Priyangga on 06/02/20.
+//  Copyright © 2020 Riyanda Febri Priyangga. All rights reserved.
+//
+
+import XCTest
+@testable import KryptoPos
+
+class KryptoPosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
